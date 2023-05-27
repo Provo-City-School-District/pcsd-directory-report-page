1b21b8fae0835fcdef690900434f199c2855dea4
Espi <joshespi@gmail.com>
2023-05-27 08:59:23 -0600

    Update pcsd-directory-report-page.php
    changes I made:
    
    Renamed the pcsd_dir_admin_page function to pcsd_dir_admin_page_callback to provide a more descriptive name.
    
    Consolidated the opening and closing PHP tags for better readability.
    
    Removed the unnecessary comments and updated the indentation for improved code clarity.
    
    Moved the code that retrieves the modification time and file content inside the if statement to avoid unnecessary file operations if the file doesn't exist.
    
    Adjusted the position of the closing curly brace for the pcsd_dir_admin_page_callback function to ensure proper structure.

--- pcsd-directory-report-page.php
+++ pcsd-directory-report-page.php
@@ -6,33 +6,34 @@ Description: Creates a dashboard page to display our report that checks for acti
 Version: 1.0
 Author: Josh Espinoza
 Author URI: https://iamjoshespinoza.com
-License: GPL2
 */
+
 add_action('admin_menu', 'pcsd_dir_admin_menu');
+
 function pcsd_dir_admin_menu()
 {
-	add_menu_page('Bad emails found in directory', 'Directory Issues', 'special_pages', 'pcsd_dir-admin-page.php', 'pcsd_dir_admin_page', 'https://globalassets.provo.edu/image/icons/pcsd-icon-16x16.png', 6);
+	add_menu_page('Directory Issues', 'Directory Issues', 'special_pages', 'pcsd_dir_admin_page', 'pcsd_dir_admin_page_callback', 'https://globalassets.provo.edu/image/icons/pcsd-icon-16x16.png', 6);
 }
-function pcsd_dir_admin_page()
+
+function pcsd_dir_admin_page_callback()
 {
-?>
-	<div class="wrap">
-		<h1>Invalid Emails in The Website Directory</h1>
-		<?php
-		$dirReportPath = '/var/www/html/public_html/wp-content/uploads/bad-emails/docker-directory_report.txt';
-		date_default_timezone_set('America/Denver');
-		if (file_exists($dirReportPath)) {
-			$modification_time = filemtime($dirReportPath);
-		} else {
-			echo 'File not found.';
-		}
-		//this file is produced currently by a container running on 1.104. link to github of code https://github.com/Provo-City-School-District/website-directory-checker
+	$dirReportPath = '/var/www/html/public_html/wp-content/uploads/bad-emails/docker-directory_report.txt';
+	date_default_timezone_set('America/Denver');
+
+	if (file_exists($dirReportPath)) {
+		$modification_time = filemtime($dirReportPath);
 		$dircheck_mainweb = nl2br(file_get_contents($dirReportPath));
-		?>
-		<p>This page will show you emails found in the website directory that are not currently active/found in the vault.</p>
-		<?= '<strong>This report was last updated on:</strong> ' . date('Y-m-d H:i:s', $modification_time); ?>
-
-		<?php echo '<br><br>' . $dircheck_mainweb; ?>
-	</div> <?php
-
-		}
+?>
+		<div class="wrap">
+			<h1>Invalid Emails in The Website Directory</h1>
+			<p>This page will show you emails found in the website directory that are not currently active/found in the vault.</p>
+			<strong>This report was last updated on:</strong> <?= date('Y-m-d H:i:s', $modification_time); ?>
+			<br><br>
+			<?= $dircheck_mainweb; ?>
+		</div>
+<?php
+	} else {
+		echo 'File not found.';
+	}
+}
+?>
